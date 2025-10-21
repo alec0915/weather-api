f88db49268ab9873b18a44aaef034f779c4a1a72
alec0915 <alecbwhyte@gmail.com>
2025-10-21 12:29:59 -0700

    moving to my pc

---
+++ weather.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+load_dotenv(Path("D:/repos/weather-api/.env"))
+print(os.getenv("KEY"))
+
+f = open("weather.json",'w')
+
+
+key = os.getenv("KEY")
+# Base URL: http://api.weatherapi.com/v1
+
+# API	API Method
+# Current weather	/current.json or /current.xml
+# Forecast	/forecast.json or /forecast.xml
+# Search or Autocomplete	/search.json or /search.xml
+# History	/history.json or /history.xml
+# Alerts	/alerts.json or /alerts.xml
+# Marine	/marine.json or /marine.xml
+# Future	/future.json or /future.xml
+# Time Zone	/timezone.json or /timezone.xml
+# Sports	/sports.json or /sports.xml
+# Astronomy	/astronomy.json or /astronomy.xml
+# IP Lookup	/ip.json or /ip.xml
+
+def getWeather(location):
+    url = "http://api.weatherapi.com/v1/current.json?key="+str(key)+"&q="+location
+    print(url)
+    response = requests.get(url)
+    
+    print('============')
+    #print(response)
+
+    weatherData = response.json()
+
+    #json.dump(weatherData,f)
+
+
+    print("In", weatherData['location']['name'])
+    print("Temperature is currently " + str(weatherData['current']['temp_f']) + "°F")
+    print("Wind is blowing", weatherData['current']['wind_mph'],"mph", weatherData['current']['wind_dir'])
+    print("It is", weatherData['current']['condition']['text'])
+    
+
+    print('============')
+    
+
+
+
+
+
+print(getWeather("New York"))
+
+
+f.close()
