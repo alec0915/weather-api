d901031c7f846b55a4ec601b0c6aab3bc683fa96
Alec Whyte <136131423+alec0915@users.noreply.github.com>
2025-11-02 20:46:34 -0800

    Venvadding (#6)
    * starting with the addition of the forecast calls
    
    * added humidity, precipitation, and UV index
    
    * Getting back into it
    
    * added a venv and a requirements.txt
    
    * using a venv now with a requirements.txt
    
    * Delete env directory
    
    * hard coded location for testing

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ import json
 import os
 import io
 from dotenv import load_dotenv
+from flask import Flask
 load_dotenv()
 print(os.getenv("KEY"))
 print(os.getenv("DIR"))
@@ -12,50 +13,80 @@ KEY = os.getenv("KEY")
 # API	API Method
 # Current weather	/current.json or /current.xml
 # Forecast	/forecast.json or /forecast.xml
-# Search or Autocomplete	/search.json or /search.xml
 # History	/history.json or /history.xml
-# Alerts	/alerts.json or /alerts.xml
-# Marine	/marine.json or /marine.xml
-# Future	/future.json or /future.xml
-# Time Zone	/timezone.json or /timezone.xml
-# Sports	/sports.json or /sports.xml
 # Astronomy	/astronomy.json or /astronomy.xml
-# IP Lookup	/ip.json or /ip.xml
 
 def getWeather(location):
-    url = "http://api.weatherapi.com/v1/current.json?key="+str(KEY)+"&q="+location
+    #url = "http://api.weatherapi.com/v1/current.json?key="+str(KEY)+"&q="+location
+    url = "http://api.weatherapi.com/v1/forecast.json?key="+str(KEY)+"&q="+location+"&days=3&aqi=yes&alerts=no"
+    
+    
+    
     #print(url)
     response = requests.get(url)
-    
-    print('============')
-    #print(response)
-
     weatherData = response.json()
 
     #f = open("weather.json",'w')
     #json.dump(weatherData,f)
     #f.close()
 
+    #f = open("forecast.json",'w')
+    #json.dump(weatherData,f)
+    #f.close()
 
-    print("In", weatherData['location']['name'])
+    print('============')
+
+    print("In", weatherData['location']['name'] + ', ' + weatherData['location']['region'] + ', ' + weatherData['location']['country'])
     print("Temperature is currently " + str(weatherData['current']['temp_f']) + "°F")
     print("Wind is blowing", weatherData['current']['wind_mph'],"mph", weatherData['current']['wind_dir'])
     print("It is", weatherData['current']['condition']['text'])
-    
+    print("Humidity:", weatherData['current']['humidity'])
+    print("Precipitation:", weatherData['current']['precip_in'],'in')
+    print("UV Index:", weatherData['current']['uv'])
+    print('\n')
+
+
+
+
+    for day in weatherData['forecast']['forecastday']:
+        print(day['date'])
+        print("Max Temp:", day['day']['maxtemp_f'])
+        print("Min Temp:", day['day']['mintemp_f'])
+        print("Condition:", day['day']['condition']['text'])
+        print("Humidity:", day['day']['avghumidity'])
+        print("Precipitation:", day['day']['totalprecip_in'],'in')
+        print("UV Index:", day['day']['uv'])
+        print(day['astro']['sunrise'], 'to', day['astro']['sunset'])
+        print(day['astro']['moonrise'], 'to', day['astro']['moonset'])
+        print(day['astro']['moon_phase'])
+        print('\n')
+        for hour in day['hour']:
+            print(hour['time'], '-', str(hour['temp_f'])+ '°F -', str(hour['condition']['text']))
+            print('Humidity:', str(hour['humidity'])+ '% ')
+            print('Precipitation:', str(hour['precip_in']) + 'in')
+            print('Wind:', str(hour['wind_mph'])+ 'mph', hour['wind_dir'])
+            print('UV Index:', hour['uv'])
+            print('\n')
+
+
 
     print('============')
-    
+
+
+
+    pass
 
 
 
 
 
-location = input("Where would you like to know the weather? (type 'exit' to quit):")
+#location = input("Where would you like to know the weather? (type 'q' to quit):")
 
-while(location != "exit"):
+location = "Los Angeles"
 
-    try:
-        getWeather(location)
-    except KeyError:
-        print("Sorry, I couldn't find the weather for that location. Please try again.")
-    location = input("Where would you like to know the weather? (type 'exit' to quit):")
+#while(location != "q"):
+try:
+    getWeather(location)
+except KeyError:
+    print("Sorry, I couldn't find the weather for that location. Please try again.")
+#location = input("Where would you like to know the weather? (type 'q' to quit):")
