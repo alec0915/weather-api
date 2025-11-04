88977a7b05d299fcd13c0a763e35dba416ffd7bb
Alec Whyte <136131423+alec0915@users.noreply.github.com>
2025-11-03 20:16:19 -0800

    Flask testing (#7)
    * Adding in Flask to test how it works
    
    * testing Flask
    
    * added index.html, testing its use with flask, and watching youtube tutorials
    
    * added weather.html, added basic weather route function
    
    * displaying weather data on page
    
    * catching key errors

--- weather.py
+++ weather.py
@@ -3,7 +3,10 @@ import json
 import os
 import io
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify,request, render_template, flash,redirect
+from markupsafe import escape
+
+
 load_dotenv()
 print(os.getenv("KEY"))
 print(os.getenv("DIR"))
@@ -16,10 +19,13 @@ KEY = os.getenv("KEY")
 # History	/history.json or /history.xml
 # Astronomy	/astronomy.json or /astronomy.xml
 
+
+
+
+
 def getWeather(location):
     #url = "http://api.weatherapi.com/v1/current.json?key="+str(KEY)+"&q="+location
     url = "http://api.weatherapi.com/v1/forecast.json?key="+str(KEY)+"&q="+location+"&days=3&aqi=yes&alerts=no"
-    
     
     
     #print(url)
@@ -34,47 +40,61 @@ def getWeather(location):
     #json.dump(weatherData,f)
     #f.close()
 
-    print('============')
+    try:
 
-    print("In", weatherData['location']['name'] + ', ' + weatherData['location']['region'] + ', ' + weatherData['location']['country'])
-    print("Temperature is currently " + str(weatherData['current']['temp_f']) + "°F")
-    print("Wind is blowing", weatherData['current']['wind_mph'],"mph", weatherData['current']['wind_dir'])
-    print("It is", weatherData['current']['condition']['text'])
-    print("Humidity:", weatherData['current']['humidity'])
-    print("Precipitation:", weatherData['current']['precip_in'],'in')
-    print("UV Index:", weatherData['current']['uv'])
-    print('\n')
+        print('============')
 
-
-
-
-    for day in weatherData['forecast']['forecastday']:
-        print(day['date'])
-        print("Max Temp:", day['day']['maxtemp_f'])
-        print("Min Temp:", day['day']['mintemp_f'])
-        print("Condition:", day['day']['condition']['text'])
-        print("Humidity:", day['day']['avghumidity'])
-        print("Precipitation:", day['day']['totalprecip_in'],'in')
-        print("UV Index:", day['day']['uv'])
-        print(day['astro']['sunrise'], 'to', day['astro']['sunset'])
-        print(day['astro']['moonrise'], 'to', day['astro']['moonset'])
-        print(day['astro']['moon_phase'])
+        print("In", weatherData['location']['name'] + ', ' + weatherData['location']['region'] + ', ' + weatherData['location']['country'])
+        print("Temperature is currently " + str(weatherData['current']['temp_f']) + "°F")
+        print("Wind is blowing", weatherData['current']['wind_mph'],"mph", weatherData['current']['wind_dir'])
+        print("It is", weatherData['current']['condition']['text'])
+        print("Humidity:", weatherData['current']['humidity'])
+        print("Precipitation:", weatherData['current']['precip_in'],'in')
+        print("UV Index:", weatherData['current']['uv'])
         print('\n')
-        for hour in day['hour']:
-            print(hour['time'], '-', str(hour['temp_f'])+ '°F -', str(hour['condition']['text']))
-            print('Humidity:', str(hour['humidity'])+ '% ')
-            print('Precipitation:', str(hour['precip_in']) + 'in')
-            print('Wind:', str(hour['wind_mph'])+ 'mph', hour['wind_dir'])
-            print('UV Index:', hour['uv'])
-            print('\n')
+
+        place = weatherData['location']['name'] + ', ' + weatherData['location']['region'] + ', ' + weatherData['location']['country']
+        temp = weatherData['current']['temp_f']
+        condition = weatherData['current']['condition']['text']
+        wind = f"{weatherData['current']['wind_mph']} mph {weatherData['current']['wind_dir']}"
+        humidity = weatherData['current']['humidity']
+        precip = weatherData['current']['precip_in']
+        uv = weatherData['current']['uv']
+        image_url = "http:" + weatherData['current']['condition']['icon']
+        localtime = weatherData['location']['localtime']
 
 
+    #    for day in weatherData['forecast']['forecastday']:
+    #        print(day['date'])
+    #        print("Max Temp:", day['day']['maxtemp_f'])
+    #        print("Min Temp:", day['day']['mintemp_f'])
+    #        print("Condition:", day['day']['condition']['text'])
+    #        print("Humidity:", day['day']['avghumidity'])
+    #        print("Precipitation:", day['day']['totalprecip_in'],'in')
+    #        print("UV Index:", day['day']['uv'])
+    #        print(day['astro']['sunrise'], 'to', day['astro']['sunset'])
+    #        print(day['astro']['moonrise'], 'to', day['astro']['moonset'])
+    #        print(day['astro']['moon_phase'])
+    #        print('\n')
+    #        for hour in day['hour']:
+    #            print(hour['time'], '-', str(hour['temp_f'])+ '°F -', str(hour['condition']['text']))
+    #            print('Humidity:', str(hour['humidity'])+ '% ')
+    #            print('Precipitation:', str(hour['precip_in']) + 'in')
+    #            print('Wind:', str(hour['wind_mph'])+ 'mph', hour['wind_dir'])
+    #            print('UV Index:', hour['uv'])
+    #            print('\n')
 
-    print('============')
 
+        package1 = (place, temp, condition, wind, humidity, precip, uv, image_url, localtime)
+        print('============')
+    except KeyError:
+        print("Location not recognized")
+        package1 = (-1,-1,-1,-1,-1,-1,-1,-1,-1)
 
+    
 
-    pass
+    return package1
+    #pass
 
 
 
@@ -85,8 +105,38 @@ def getWeather(location):
 location = "Los Angeles"
 
 #while(location != "q"):
-try:
-    getWeather(location)
-except KeyError:
-    print("Sorry, I couldn't find the weather for that location. Please try again.")
+
 #location = input("Where would you like to know the weather? (type 'q' to quit):")
+
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24).hex()
+
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        location = request.form.get('location')
+        if not location:
+            print("No location provided")
+            flash("Please enter a location")
+            return render_template('index.html')
+        print('Received location: '+location)
+        return redirect('/weather/'+location)
+    return render_template('index.html')
+#https://www.youtube.com/watch?v=hHkl7bKZOCI
+
+
+@app.route('/weather/<location>')
+def weather(location):
+    weatherdata=getWeather(location)
+    if weatherdata[0] == -1:
+        flash("Location not recognized. Please try again.")
+        return redirect('/')
+    place, temp, condition, wind, humidity, precip, uv, image_url, localtime = weatherdata
+    return render_template('weather.html', location=escape(location), place=place, temp=temp, condition=condition, wind=wind, humidity=humidity, precip=precip, uv=uv, image_url=image_url, localtime=localtime)
+
+if __name__ == '__main__':
+    app.run(debug=True)
